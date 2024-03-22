35edf0edd638dc83f11b66d51ffede701f9709d5
Adebola <adebola@poptin.com>
2024-03-22 12:16:18 +0100

    total transaction, total income, total expenses implementation

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,48 @@
   import IncomeExpenses from './components/IncomeExpenses.vue';
   import TransactionList from './components/TransactionList.vue';
   import AddTransaction from './components/AddTransaction.vue';
+
+  import { ref, computed } from 'vue';
+
+  const transactions = ref([
+    { id: 1, text: 'Flower', amount: -19.99},
+    { id: 2, text: 'Salary', amount: 299.97},
+    { id: 3, text: 'Book', amount: -50},
+    { id: 4, text: 'Camera', amount: 150},
+  ]);
+
+  // Get total
+  const total = computed(() => {
+    return transactions.value.reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0);
+  });
+
+  // Get Income
+  const income = computed(() => {
+    return transactions.value
+      .filter((transaction) => transaction.amount > 0)
+      .reduce((acc, transaction) => {
+        return acc + transaction.amount;
+      }, 0).toFixed(2);
+  });
+
+  // Get Expenses
+  const expenses = computed(() => {
+    return transactions.value
+      .filter((transaction) => transaction.amount < 0)
+      .reduce((acc, transaction) => {
+        return acc + transaction.amount;
+      }, 0).toFixed(2);
+  });
 </script>
 
 <template>
   <Header />
   <div class="container">
-    <Balance />
-    <IncomeExpenses />
-    <TransactionList />
+    <Balance :total="total" />
+    <IncomeExpenses :income="parseInt(income)" :expenses="parseInt(expenses)" />
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,4 +1,13 @@
+<script setup>
+    const { total } = defineProps({
+        total: {
+            type: Number,
+            required: true
+        }
+    })
+</script>
+
 <template>
     <h4>Your Balance</h4>
-    <h1 id="balance">$0.00</h1>
+    <h1 id="balance">${{ total }}</h1>
 </template>
